0235e91df45c9545647b2bdfb3a772122265feaa
sachin39 <sachin.kr.39@gmail.com>
2022-02-21 18:57:08 +0530

    illustration of reactive web & reactive mongodb|corrected folder structure

---
+++ src/test/java/com/reactiveprogramming/SpringReactiveMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactiveprogramming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
